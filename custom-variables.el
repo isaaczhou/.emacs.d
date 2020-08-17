7ce97903b7b3245afdec06c63ec142cce02d1355
isaaczhou <isaaczhou85@gmail.com>
2020-08-17 08:14:43 -0400

    update set-frame-font

--- custom-variables.el
+++ custom-variables.el
@@ -8,7 +8,7 @@
     ("890a1a44aff08a726439b03c69ff210fe929f0eff846ccb85f78ee0e27c7b2ea" default)))
  '(package-selected-packages
    (quote
-    (go-rename go-guru go-eldoc lsp-ui lsp-mode ob-ipython yasnippet leuven-theme org-bullets auto-compile use-package))))
+    (pythonic irony-eldoc flycheck-irony company-irony company-irony-c-headers ob-ipython yasnippet leuven-theme org-bullets auto-compile use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
