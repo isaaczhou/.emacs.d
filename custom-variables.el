31703ac37fc67bb1ffbce982dd546801c4909020
isaaczhou <isaaczhou85@gmail.com>
2020-08-18 12:20:13 -0400

    add all the icons

--- custom-variables.el
+++ custom-variables.el
@@ -4,11 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("890a1a44aff08a726439b03c69ff210fe929f0eff846ccb85f78ee0e27c7b2ea" default)))
+   '("890a1a44aff08a726439b03c69ff210fe929f0eff846ccb85f78ee0e27c7b2ea" default))
  '(package-selected-packages
-   (quote
-    (pythonic irony-eldoc flycheck-irony company-irony company-irony-c-headers ob-ipython yasnippet leuven-theme org-bullets auto-compile use-package))))
+   '(all-the-icons-dired all-the-icons-ivy-rich all-the-icons-ivy ob-ipython yasnippet leuven-theme org-bullets auto-compile use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
