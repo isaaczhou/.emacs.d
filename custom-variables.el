0af956b8f5f666a66b58022fea36bf14b4b6cbe8
isaaczhou <isaaczhou85@gmail.com>
2020-10-22 09:17:06 -0400

    centos update

---
+++ custom-variables.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("890a1a44aff08a726439b03c69ff210fe929f0eff846ccb85f78ee0e27c7b2ea" default))
+ '(package-selected-packages
+   '(astyle ob-ipython yasnippet leuven-theme org-bullets auto-compile use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
