959668febabf220651da813256cb7d6a494ee95e
isaaczhou <isaaczhou85@gmail.com>
2020-10-31 01:52:21 -0400

    update macos

--- custom-variables.el
+++ custom-variables.el
@@ -5,11 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("890a1a44aff08a726439b03c69ff210fe929f0eff846ccb85f78ee0e27c7b2ea" default)))
+    ("2d40e8de2c676ec749cf13a0a48e35a8b678a056dc397ffe8de4424dfa69d460" "890a1a44aff08a726439b03c69ff210fe929f0eff846ccb85f78ee0e27c7b2ea" default)))
  '(irony-additional-clang-options (quote ("-std=c++11")))
  '(package-selected-packages
    (quote
-    (irony-eldoc flycheck-irony company-irony company-irony-c-headers go-rename go-guru go-eldoc ob-ipython yasnippet leuven-theme org-bullets auto-compile use-package))))
+    (ob-ipython yasnippet leuven-theme org-bullets auto-compile use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
