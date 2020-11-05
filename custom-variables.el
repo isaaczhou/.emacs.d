70a6158345546048c82a54c5dff35c9ec1c642c1
isaaczhou <isaaczhou85@gmail.com>
2020-11-05 07:52:08 -0500

    update

--- custom-variables.el
+++ custom-variables.el
@@ -4,9 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("890a1a44aff08a726439b03c69ff210fe929f0eff846ccb85f78ee0e27c7b2ea" default))
+   '("2d40e8de2c676ec749cf13a0a48e35a8b678a056dc397ffe8de4424dfa69d460" "890a1a44aff08a726439b03c69ff210fe929f0eff846ccb85f78ee0e27c7b2ea" default))
  '(package-selected-packages
-   '(counsel-projectile company-web ob-ipython yasnippet leuven-theme org-bullets auto-compile use-package)))
+   '(company-rtags eproject ob-ipython yasnippet leuven-theme org-bullets auto-compile use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
