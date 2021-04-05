972edd72766cd8e3a8ec56b5d28e903338ee0665
Isaac Zhou <isaac@isaacs-mbp.myfiosgateway.com>
2021-04-05 13:31:42 -0400

    configured c++

---
+++ custom-variables.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("039c01abb72985a21f4423dd480ddb998c57d665687786abd4e16c71128ef6ad" "890a1a44aff08a726439b03c69ff210fe929f0eff846ccb85f78ee0e27c7b2ea" default)))
+ '(irony-additional-clang-options (quote ("-std=c++11")))
+ '(package-selected-packages
+   (quote
+    (go-rename go-guru go-eldoc ob-ipython yasnippet leuven-theme org-bullets auto-compile use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
